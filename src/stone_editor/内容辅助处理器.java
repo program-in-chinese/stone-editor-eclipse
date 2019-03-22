673ec23f564e9620c2bb858140b4084b24869100
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-21 23:44:50 -0700

    重命名

--- src/stone_editor/内容辅助处理器.java
+++ src/stone_editor/内容辅助处理器.java
@@ -13,21 +13,21 @@ import org.eclipse.jface.text.contentassist.IContextInformationValidator;
 
 public class 内容辅助处理器 implements IContentAssistProcessor {
 
-  public static final String[] PROPOSALS = new String[] {"新建"};
+  public static final String[] 所有建议 = new String[] {"新建"};
 
   @Override
   public ICompletionProposal[] computeCompletionProposals(ITextViewer 视图, int 偏移) {
 
-    IDocument document = 视图.getDocument();
+    IDocument 文件 = 视图.getDocument();
 
     try {
-      int 偏移所在行 = document.getLineOfOffset(偏移);
-      int 行头偏移 = document.getLineOffset(偏移所在行);
+      int 偏移所在行 = 文件.getLineOfOffset(偏移);
+      int 行头偏移 = 文件.getLineOffset(偏移所在行);
 
       int 当前行文本长度 = 偏移 - 行头偏移;
-      String 当前行文本 = document.get(行头偏移, 当前行文本长度).toLowerCase();
+      String 当前行文本 = 文件.get(行头偏移, 当前行文本长度).toLowerCase();
 
-      return Arrays.asList(PROPOSALS).stream()
+      return Arrays.asList(所有建议).stream()
           .filter(建议 -> !视图.getDocument().get().contains(建议) && 建议.toLowerCase().startsWith(当前行文本))
           .map(建议 -> new CompletionProposal(建议, 行头偏移, 当前行文本长度, 建议.length()))
           .toArray(ICompletionProposal[]::new);
